52518733d9a5d47394772a98d5c3857b7eff3692
Gabriel <gabewilson42@gmail.com>
2018-09-28 19:31:20 -0500

    Initializing the git repository

---
+++ src/test/java/com/foreflight/airportinfo/airportinfo/AirportInfoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.foreflight.airportinfo.airportinfo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AirportInfoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
